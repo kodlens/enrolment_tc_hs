f03382d81d78a247feeface13967cb212a748db9
etienne wayne <etiennewayne@gmail.com>
2023-08-12 09:49:13 +0800

    initial stage

---
+++ database/seeders/SemesterSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SemesterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        $data = [
+            [
+                'semester' => '1ST SEMESTER',
+            ],
+            [
+                'semester' => '2ND SEMESTER',
+            ],
+
+        ];
+
+        \App\Models\Strand::insertOrIgnore($data);
+    }
+}
